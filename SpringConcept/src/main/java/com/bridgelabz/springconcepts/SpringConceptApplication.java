b77d02a2d8f7f3e5336a317e50c4ea7d87c6cbb3
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-04 20:45:23 +0530

    ADDED: Spring Concept Project

---
+++ SpringConcept/src/main/java/com/bridgelabz/springconcepts/SpringConceptApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptApplication.class, args);
+	}
+
+}
