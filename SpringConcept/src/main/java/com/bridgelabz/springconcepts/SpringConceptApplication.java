f45490e883e70202e81860417ff256c324cdd9e0
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-05 00:17:33 +0530

    ADDED: Built in SLF4J use Simple LO]ogger FramWork

--- SpringConcept/src/main/java/com/bridgelabz/springconcepts/SpringConceptApplication.java
+++ SpringConcept/src/main/java/com/bridgelabz/springconcepts/SpringConceptApplication.java
@@ -1,14 +1,22 @@
 package com.bridgelabz.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.bridgelabz.springconcepts.component.DemoBean;
 
 @SpringBootApplication
 public class SpringConceptApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptApplication.class);
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Domo");
-		SpringApplication.run(SpringConceptApplication.class, args);
+		logger.debug("Welcome to Spring Concept Domo");
+		ApplicationContext context = SpringApplication.run(SpringConceptApplication.class, args);
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
+		logger.debug("\n*** example of logger ***");
 	}
 
 }
