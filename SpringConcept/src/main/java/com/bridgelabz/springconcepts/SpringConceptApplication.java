dfa038e096d19825239230e18cb436a484fc094f
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-11-04 20:47:35 +0530

    ADDED: Spring Boot Project

--- SpringConcept/src/main/java/com/bridgelabz/springconcepts/SpringConceptApplication.java
+++ SpringConcept/src/main/java/com/bridgelabz/springconcepts/SpringConceptApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringConceptApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Spring Concept Domo");
 		SpringApplication.run(SpringConceptApplication.class, args);
 	}
 
